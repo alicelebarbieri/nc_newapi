ffd0527338eda2b3fd371ef99ae3cc48be263722
alicelebarbieri <alicele.barbieri@gmail.com>
2025-05-05 18:03:51 +0100

    implement GET /api/topics endpoint

---
+++ __tests__/topics.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+
+
+describe("GET /api/topics", () => {
+  test("200: responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.topics)).toBe(true);
+        expect(body.topics.length).toBeGreaterThan(0);
+        body.topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug", expect.any(String));
+          expect(topic).toHaveProperty("description", expect.any(String));
+        });
+      });
+  });
+});
+
+afterAll(() => {
+    return db.end();
+  });
