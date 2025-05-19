d325c0a7be9876bab68e913bd36b8ececfc2d88a
alicelebarbieri <alicele.barbieri@gmail.com>
2025-05-19 17:04:12 +0100

    12: Add comment_count to GET /api/articles/:article_id

--- models/articles.model.js
+++ models/articles.model.js
@@ -3,12 +3,21 @@ const db = require("../db/connection");
 exports.selectArticleById = (article_id) => {
   return db
     .query(
-      `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url
+      `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic,
+              articles.created_at, articles.votes, articles.article_img_url,
+              COUNT(comments.comment_id)::INT AS comment_count
        FROM articles
-       WHERE article_id = $1;`,
+       LEFT JOIN comments ON comments.article_id = articles.article_id
+       WHERE articles.article_id = $1
+       GROUP BY articles.article_id;`,
       [article_id]
     )
-    .then(({ rows }) => rows[0]);
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      return rows[0];
+    });
 };
 
 exports.selectAllArticles = (sort_by = "created_at", order = "desc", topic) => {
